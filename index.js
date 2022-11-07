15b3a466b4df8a786387ee7f7df8341c612cb3cf
Alex Castillo <alex@neurosity.co>
2022-11-07 14:09:46 -0800

    FEAT: print device info

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const { email, password, deviceId } = require("./inputs");
 (async function main() {
   const mind = new Notion(deviceId ? { deviceId } : {});
   await mind.login({ email, password }).catch(console.error);
+  const deviceInfo = await mind.getInfo();
 
   // mind.brainwaves("psd")
   mind.brainwaves("raw").subscribe((brainwaves) => {
     console.log(brainwaves);
+    console.log("deviceInfo", deviceInfo);
   });
 })();
